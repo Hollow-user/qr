ab5fff10d49ae086d20959dee20e76d97367de69
Hollow-user <eduardostanin94@gmail.com>
2019-06-24 22:59:55 +1000

    добавил счетчик поситителей на кнопках в лекциях

--- testpages/views.py
+++ testpages/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import *
 
@@ -12,13 +12,28 @@ def qr_page(request):
 
 def lecture_page(request):
 
-    lectures = Lecture.objects.all()
+    lectures = reversed(Lecture.objects.all())
+    if request.method == 'GET':
 
-    return render(request, 'testpages/lecture.html', context={'lectures':lectures})
+        return render(request, 'testpages/lecture.html', context={'lectures': lectures})
+    if request.method == 'POST':
+        a = request.POST
+        c = Lecture.objects.get(id=a['Лекция']).visiters
+        Lecture.objects.filter(id=a['Лекция']).update(visiters=c+1)
+
+        print(c)
+
+
+
+
+
+
+        return redirect('thx_url')
 
 # Функция для отображения страницы с лекциями
 
 def thx_page(request):
+
     return render(request, 'testpages/thx.html')
 
 # Функция для отображения страницы с благодарностью
