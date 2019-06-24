6413f9c0caaed1700f1c2498933242ff81623126
Hollow-user <eduardostanin94@gmail.com>
2019-06-25 02:24:45 +1000

    доработал код

--- testpages/views.py
+++ testpages/views.py
@@ -3,37 +3,25 @@ from django.http import HttpResponse
 from .models import *
 
 
-# Create your views here.
-
 def qr_page(request):
+    """Функция для отображения страницы с кр кодом"""
     return render(request, 'testpages/qr_page.html')
 
-# Функция для отображения страницы с кр кодом
 
 def lecture_page(request):
-
+    """Функция для отображения страницы с лекциями"""
     lectures = reversed(Lecture.objects.all())
     if request.method == 'GET':
-
         return render(request, 'testpages/lecture.html', context={'lectures': lectures})
     if request.method == 'POST':
         a = request.POST
         c = Lecture.objects.get(id=a['Лекция']).visiters
         Lecture.objects.filter(id=a['Лекция']).update(visiters=c+1)
-
-        print(c)
-
-
-
-
-
-
         return redirect('thx_url')
 
-# Функция для отображения страницы с лекциями
 
 def thx_page(request):
-
+    """Функция для отображения страницы с благодарностью"""
     return render(request, 'testpages/thx.html')
 
-# Функция для отображения страницы с благодарностью
+
