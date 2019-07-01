92ac79529f01e7cbc8a195a7f10ceb53bea545da
Hollow-user <eduardostanin94@gmail.com>
2019-07-02 01:40:14 +1000

    добработал некоторые вещи

--- testpages/urls.py
+++ testpages/urls.py
@@ -8,6 +8,7 @@ from .views import *
 urlpatterns = [
     path('', qr_page, name='qr_page_url'),
     path('thx/', thx_page, name='thx_url'),
+    path('check/', check_page, name='check_url'),
     path('lecture/', lecture_page, name='lecture_url'),
     path('test/', test_page, name='test_url'),
     path('test/<int:id>/', test_id_page, name='test_id_page'),
